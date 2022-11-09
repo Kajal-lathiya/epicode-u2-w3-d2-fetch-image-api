b7eba7ba41f06f7292171b2f4759908ee91d0728
Kajal lathiya <kajalbodarya77@gmail.com>
2022-11-09 18:44:21 +0530

    change design

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 const loadImageButton = document.getElementById('load-images');
+const SecondaryImage = document.getElementById('SecondaryImage');
 const cardNode = document.querySelector('.card');
 const cardBody = document.querySelector('.card-body');
-const svgTage = document.getElementsByTagName('svg');
 
-loadImageButton.addEventListener('click', () => {})
+loadImageButton.addEventListener('click', () => {
+    APIcall('Nature');
+})
 
-APIcall = () => {
+SecondaryImage.addEventListener('click', () => {
+    APIcall('Tigers');
+})
+
+APIcall = (value) => {
     let myHeaders = new Headers();
     myHeaders.append("Authorization", "Bearer 563492ad6f9170000100000186bad3b4d9e44f52971f217b0fc156d2");
 
@@ -14,33 +20,21 @@ APIcall = () => {
         headers: myHeaders,
         redirect: 'follow'
     };
-
-    fetch("https://api.pexels.com/v1/search?query=Nature", requestOptions)
+    fetch(`https://api.pexels.com/v1/search?query=${value}`, requestOptions)
         .then(response => response.json())
         .then(result => {
             const { photos } = result;
-            console.log('svgTage-->', svgTage);
+
             for (let i = 0; i < photos.length; i++) {
                 const obj = photos[i];
-                console.log('obj.src.original->', obj.src.original);
-                var img = document.createElement("img");
-                // img.setAttribute("Sun", "im");
+                let img = document.createElement("img");
                 img.src = obj.src.original;
                 img.alt = 'image'
-
-                // this.parentNode.replaceChild(img, svgTage);
-
-                // cardNode.insertBefore(userDiv, cardBody);
-                // cardNode.appendChild(userDiv)
-                // svgTage.replaceWith(img);
-                parentDiv.replaceChild(sp1, sp2);
-
+                let svgTage = document.querySelector('.card-img-top');
+                svgTage.replaceWith(img);
             }
         })
         .catch(error => console.log('error', error));
-
 }
 
-window.onload = () => {
-    APIcall();
-}
+window.onload = () => {}
